5b80fd95c3da9a8366f04c9882bb1f9291960911
ALok Pandey <114446066+alok-07-pandey@users.noreply.github.com>
2024-11-29 19:20:12 +0000

    Variables Done

---
+++ .devcontainer/01_Basics/01_Variables.js
@@ -0,0 +1,27 @@
+const userId = 73521998;
+var name = "Alok";
+let nickName = "Babu";
+const address = "Satna";
+let game = "Football";
+game = "Cricket";
+name = "Sonakshi"
+nickName = "Alu";
+usedId = 998;
+/*
+Prefer not to use var
+because of issue in  block space in functionl scope
+
+*/
+
+
+
+//var name = "Sonakshi";
+
+console.log(userId);
+console.log(name);
+console.log(nickName);
+console.log(address);
+console.log(game);
+console.table([userId,name,nickName,address,game]);
+
+
