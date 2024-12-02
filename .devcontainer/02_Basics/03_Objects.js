9de8dc4e320ea52a64d2c9801b01481cc6240ad9
ALok Pandey <114446066+alok-07-pandey@users.noreply.github.com>
2024-12-02 18:36:43 +0000

    day 3 done

--- .devcontainer/02_Basics/03_Objects.js
+++ .devcontainer/02_Basics/03_Objects.js
@@ -1,37 +1,39 @@
 // // singleton 
 // // Object.crete
 // const mysym = Symbol("Key111")
-const obj = {
-    name: "Alok",
-    fullnmae : "Alok Pandey",
-   // [mysym]  : "MyKEYYYYYY",
-    id : 12209344,
-    location: "Satna",
-    Stream: "CSE",
-    UG : "B-Tech",
-    DreamCompany :"Microsoft"
-}
-// console.log(obj.id);
-// console.log(obj["id"]);
-// console.log(obj.fullnmae);
-// //console.log(obj."fullnmae");
-// console.log( typeof obj.mysym)
+// const obj = {
+//     name: "Alok",
+//     fullnmae : "Alok Pandey",
+//    // [mysym]  : "MyKEYYYYYY",
+//     id : 12209344,
+//     location: "Satna",
+//     Stream: "CSE",
+//     UG : "B-Tech",
+//     DreamCompany :"Microsoft"
+// }
+// // console.log(obj.id);
+// // console.log(obj["id"]);
+// // console.log(obj.fullnmae);
+// // //console.log(obj."fullnmae");
+// // console.log( typeof obj.mysym)
 
-// obj.name = "Sonakshi"
-// Object.freeze(obj)
-// obj.name = "babu"
-// console.log(obj);
-
-
-// funtion in object 
-
-obj.greeting = function(){
-    console.log("Hello Alok ")
-}
-obj.greeting2 = function(){
-    console.log(`my name is, ${this.name}`)
-}
+// // obj.name = "Sonakshi"
+// // Object.freeze(obj)
+// // obj.name = "babu"
+// // console.log(obj);
 
 
-console.log(obj.greeting2())
+// // funtion in object 
+
+// obj.greeting = function(){
+//     console.log("Hello Alok ")
+// }
+// obj.greeting2 = function(){
+//     console.log(`my name is, ${this.name}`)
+// }
+
+
+// console.log(obj.greeting2())/
+
+
 
