4e6cd27fdc660aea26042a4add2d18748cf03ce4
ALok Pandey <114446066+alok-07-pandey@users.noreply.github.com>
2024-12-04 23:59:50 +0530

    day 5

---
+++ .devcontainer/04_Objects.js
@@ -0,0 +1,76 @@
+// const tinder = new Object(); // Singleton object
+// const tinder2 = {}  // non singleto object
+// tinder2.name = "Alok Pandey"
+// tinder2.Id = 1234
+// tinder2.Email = "Alok00007pandey@gmail.com"
+// console.table(tinder2);  
+
+// const regularUser = {
+//     name : "Mahadev",
+//     Age : " Infinity",
+//     Addres: "Kan Kan me ",
+//     id : 0,
+//     fullname :{
+        
+//             username :{
+//             firstname: "Mahadev",
+//             lastname : "Pandey"
+//             }
+        
+//     }
+// }
+// console.table(regularUser.fullname)
+// const obj1 = {1 : "a", 2: "b", 3 : "c"}
+// const obj2 = {4 : "d", 5: "e", 6:"f"}
+
+// const obj3 = {...obj1 , ...obj2} // spread Operator 
+// console.log(obj3)
+// console.table(obj3)
+
+// Array object 
+
+// const user [
+//     {
+//         name : "Sanjay",
+//         age :  20,
+//         address : "rajasthan"
+//     },
+//     {
+//         name : " Alok",
+//         age : 19,
+//         address : "Satna Bihar"
+//     }
+// ]
+// console.log(user.name)
+
+// const obj = {
+//     name : "Alok",
+  
+//     book : " Rich Dad Poor Dad",
+//     game : " Football",
+//     courseInstructor : "Ravi Kant Sahu"
+// }
+
+
+// const {courseInstructor : tutor} = obj  // Destructor
+// console.log(tutor)
+
+// // Api 
+// Json 
+{
+    name : "Alok",
+    class : "Six Sem Student",
+    minor : "FullStack"
+}
+{
+     // Json in Object
+}
+{
+
+}
+
+[
+    {
+        // Json in Array
+    }
+]
